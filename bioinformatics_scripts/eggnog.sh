e79765621e05c5367a6b2d2d67dacaa8f711c9de
Cynthia Becker <54375374+CynthiaBecker@users.noreply.github.com>
2024-08-01 15:45:51 -0400

    fix eggnog script

--- bioinformatics_scripts/eggnog.sh
+++ bioinformatics_scripts/eggnog.sh
@@ -2,28 +2,21 @@
 #SBATCH --job-name=eggnog_%j
 #SBATCH --output=logs/eggnog_%j.log
 #SBATCH --error=logs/eggnog_%j.err
-#SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user cynthiabecker@ufl.edu
 #SBATCH --mem-per-cpu=2gb
 #SBATCH --cpus-per-task=100
-#SBATCH --time=4-00:00:00
-#SBATCH --account=juliemeyer
-#SBATCH --qos=juliemeyer-b
 
 export OMP_NUM_THREADS=100
 
-# run this script from /blue/juliemeyer/cynthiabecker/ofav_mags/ with "sbatch scripts/eggnog.sh"
-
-# load eggnog-mapper and print version
+# load eggnog-mapper which is a module on the HPC and print version
 module load eggnog-mapper/2.1.6
 emapper.py --version
 
 # make a directory to house results 
-mkdir /blue/juliemeyer/share/coassembly_rrc_ofav/eggnog_output/prodigal
-cd /blue/juliemeyer/share/coassembly_rrc_ofav/eggnog_output/prodigal
+mkdir /path/to/coassembly/coassembly_rrc_ofav/eggnog_output
+cd /path/to/coassembly/coassembly_rrc_ofav/eggnog_output
 
 # run eggnog-mapper on the complete FLK co-assembly
-emapper.py -i /blue/juliemeyer/share/coassembly_rrc_ofav/prodigal_output/FLK_OFAV_MG_pred.faa \
+emapper.py -i /path/to/coassembly/coassembly_rrc_ofav/prodigal_output/FLK_OFAV_MG_pred.faa \
     --itype proteins \
     --data_dir /data/reference/eggnog-mapper/2.1.6 \
     -o FLK_OFAV_MG \
